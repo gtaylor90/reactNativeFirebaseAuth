77ddae94a32cbc6848a12feff6e958d64e196f59
gtaylor90 <graham.taylor.90@gmail.com>
2017-04-27 22:17:47 -0500

    log in form stuff and some syntax fixes

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { View } from 'react-native';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyCej4wwo6tswqodzw-ehUja_5MMSqaNGd4',
+    authDomain: 'auth-a10c9.firebaseapp.com',
+    databaseURL: 'https://auth-a10c9.firebaseio.com',
+    projectId: 'auth-a10c9',
+    storageBucket: 'auth-a10c9.appspot.com',
+    messagingSenderId: '8567717104'
+  });
+  }
+
   render() {
     return (
       <View>
         <Header headerText="Authentication" />
+        <LoginForm />
       </View>
     );
   }
